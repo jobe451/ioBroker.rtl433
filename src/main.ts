0811626e4075a61f4ac132a01443f26a0adaa4b7
Daniel Keller <jobe451@gmail.com>
2020-08-06 22:44:03 +0200

    first version

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
+import {spawn, ChildProcessWithoutNullStreams} from "child_process";
+
+enum stdInReadState {
+	START,
+	IN_JSON,
+	INBETWEEN_JSON,
+	KILL,
+}
+
+const procRestartTimeoutSeconds = 10;
+
 /*
- * Created with @iobroker/create-adapter v1.26.0
+ * Created with @iobroker/create-adapter v1.24.2
  */
 
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 import * as utils from "@iobroker/adapter-core";
+
 
 // Load your modules here, e.g.:
 // import * as fs from "fs";
@@ -16,156 +28,186 @@ declare global {
 	namespace ioBroker {
 		interface AdapterConfig {
 			// Define the shape of your options here (recommended)
-			option1: boolean;
-			option2: string;
 			// Or use a catch-all approach
 			[key: string]: any;
 		}
 	}
 }
 
-class Rtl433 extends utils.Adapter {
-
+class Misol extends utils.Adapter {
+	private readState: stdInReadState;
+	private openParentCount: number;
+	private currentMsg: string;
+	private proc: ChildProcessWithoutNullStreams | null;
+	
 	public constructor(options: Partial<utils.AdapterOptions> = {}) {
 		super({
 			...options,
-			name: "rtl433",
+			name: "neeo",
 		});
 		this.on("ready", this.onReady.bind(this));
+		this.on("objectChange", this.onObjectChange.bind(this));
 		this.on("stateChange", this.onStateChange.bind(this));
-		// this.on("objectChange", this.onObjectChange.bind(this));
-		// this.on("message", this.onMessage.bind(this));
+		//this.on("message", this.onMessage.bind(this));
 		this.on("unload", this.onUnload.bind(this));
+
+		this.readState = stdInReadState.START;
+		this.openParentCount = 0;
+		this.currentMsg = "";
+		this.proc = null;
 	}
 
 	/**
 	 * Is called when databases are connected and adapter received configuration.
 	 */
 	private async onReady(): Promise<void> {
-		// Initialize your adapter here
 
-		// Reset the connection indicator during startup
 		this.setState("info.connection", false, true);
 
-		// The adapters config (in the instance object everything under the attribute "native") is accessible via
-		// this.config:
-		this.log.info("config option1: " + this.config.option1);
-		this.log.info("config option2: " + this.config.option2);
+		this.init();
 
-		/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-		*/
-		await this.setObjectNotExistsAsync("testVariable", {
-			type: "state",
-			common: {
-				name: "testVariable",
-				type: "boolean",
-				role: "indicator",
-				read: true,
-				write: true,
-			},
-			native: {},
+		// in this template all states changes inside the adapters namespace are subscribed
+		this.subscribeStates("*");
+	}
+
+	private init(): void {
+		this.readState = stdInReadState.START;
+		this.openParentCount = 0;
+		this.currentMsg = "";
+	
+		this.proc = spawn("rtl_433", ["-F", "json",  "-M", "utc", "-M", "level", "-M", "protocol"]);
+
+		this.proc.stdout.on("data", (data: Buffer) => {
+			this.evalStdInData(data);
 		});
 
-		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		this.subscribeStates("testVariable");
-		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-		// this.subscribeStates("lights.*");
-		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-		// this.subscribeStates("*");
-
-		/*
-			setState examples
-			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-		*/
-		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync("testVariable", true);
-
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync("testVariable", { val: true, ack: true });
-
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
-
-		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync("admin", "iobroker");
-		this.log.info("check user admin pw iobroker: " + result);
-
-		result = await this.checkGroupAsync("admin", "admin");
-		this.log.info("check group user admin group admin: " + result);
+		this.proc.stderr.on("data", (data: Buffer) => {
+			const dataStr = data.toString();
+			let isError = false;
+			// todo also if "error" is mentioned
+			if (this.readState !== stdInReadState.START || dataStr === "Async read stalled, exiting!") {
+				this.log.error(dataStr);
+				isError = true;
+			}
+			else {
+				this.log.info(dataStr);
+			}
+			
+			if (isError && this.readState !== stdInReadState.KILL) {
+				if (this.proc) {
+					console.log("killing the process");
+					this.proc.kill();
+				}					
+			}
+		});
+			
+		this.proc.on("close", (code: any, signal: any) => {
+			this.log.info(`rtl_433 closed with code ${code}, signal ${signal}, restarting in ${procRestartTimeoutSeconds} seconds`);
+			this.setState("info.connection", false, true);
+			
+			setTimeout(() => {
+				this.init();
+				console.log("restarting process")
+			}, procRestartTimeoutSeconds)
+		});
 	}
+	
+	private evalStdInData(data: Buffer): void {
+		for (const char of data.toString()) {
+	
+			if (this.readState != stdInReadState.IN_JSON) {
+				if (char === "{") {
+					this.readState = stdInReadState.IN_JSON;
+					this.setState("info.connection", true, true);
+					this.currentMsg = char;
+					this.openParentCount = 1;
+				}
+			}
+	
+			else if (this.readState = stdInReadState.IN_JSON) {
+				this.currentMsg += char;
+	
+				if (char === "{") {
+					this.openParentCount++;
+				}
+				else if (char === "}") {
+					this.openParentCount--;
+				}
+	
+				if (this.openParentCount ===  0) {
+					this.readState = stdInReadState.INBETWEEN_JSON;
+					try {
+						console.log("json")
+						const msg = JSON.parse (this.currentMsg);
+						this.handleData(msg);
+					}
+					catch (e) {
+						console.log ("parser error", e);
+					}
+					finally {
+						this.currentMsg = "";
+					}
+				}
+			}
+		}
+	
+		if (this.readState === stdInReadState.START) {
+			console.log (data.toString());
+		}
+	}
+	
+	private handleData(data: any) {
+
+		const path = "devices." + data.model + ".";
+		for (const key in data) {
+			if (key === "model") {
+				continue;
+			}
+			this.setStateAsync(path + key, { val: data[key], ack: true });
+		}
+
+		this.setStateAsync("lastUpdate", { val: data.time, ack: true });
+	}
+	
+	
 
 	/**
 	 * Is called when adapter shuts down - callback has to be called under any circumstances!
 	 */
 	private onUnload(callback: () => void): void {
 		try {
-			// Here you must clear all timeouts or intervals that may still be active
-			// clearTimeout(timeout1);
-			// clearTimeout(timeout2);
-			// ...
-			// clearInterval(interval1);
-
+			this.log.info("cleaned everything up...");
 			callback();
 		} catch (e) {
 			callback();
 		}
 	}
 
-	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-	// /**
-	//  * Is called if a subscribed object changes
-	//  */
-	// private onObjectChange(id: string, obj: ioBroker.Object | null | undefined): void {
-	// 	if (obj) {
-	// 		// The object was changed
-	// 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-	// 	} else {
-	// 		// The object was deleted
-	// 		this.log.info(`object ${id} deleted`);
-	// 	}
-	// }
+	/**
+	 * Is called if a subscribed object changes
+	 */
+	private onObjectChange(id: string, obj: ioBroker.Object | null | undefined): void {
+		if (obj) {
+			// The object was changed
+			this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+		} else {
+			// The object was deleted
+			this.log.info(`object ${id} deleted`);
+		}
+	}
 
 	/**
 	 * Is called if a subscribed state changes
 	 */
 	private onStateChange(id: string, state: ioBroker.State | null | undefined): void {
-		if (state) {
-			// The state was changed
-			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-		} else {
-			// The state was deleted
-			this.log.info(`state ${id} deleted`);
-		}
+		this.log.info(`state ${id} changed: ${state}`);
 	}
-
-	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-	// /**
-	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-	//  * Using this method requires "common.message" property to be set to true in io-package.json
-	//  */
-	// private onMessage(obj: ioBroker.Message): void {
-	// 	if (typeof obj === "object" && obj.message) {
-	// 		if (obj.command === "send") {
-	// 			// e.g. send email or pushover or whatever
-	// 			this.log.info("send command");
-
-	// 			// Send response in callback if required
-	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-	// 		}
-	// 	}
-	// }
-
 }
 
 if (module.parent) {
 	// Export the constructor in compact mode
-	module.exports = (options: Partial<utils.AdapterOptions> | undefined) => new Rtl433(options);
+	module.exports = (options: Partial<utils.AdapterOptions> | undefined) => new Misol(options);
 } else {
 	// otherwise start the instance directly
-	(() => new Rtl433())();
+	(() => new Misol())();
 }
