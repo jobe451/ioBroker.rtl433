63242fb1f0c6953c8b901b58e6a27aad2f5664e1
Daniel Keller <jobe451@gmail.com>
2020-08-06 04:10:55 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"rtl433 adapter settings": {
+		"en": "Adapter settings for rtl433",
+		"de": "Adaptereinstellungen für rtl433",
+		"ru": "Настройки адаптера для rtl433",
+		"pt": "Configurações do adaptador para rtl433",
+		"nl": "Adapterinstellingen voor rtl433",
+		"fr": "Paramètres d'adaptateur pour rtl433",
+		"it": "Impostazioni dell'adattatore per rtl433",
+		"es": "Ajustes del adaptador para rtl433",
+		"pl": "Ustawienia adaptera dla rtl433",
+		"zh-cn": "rtl433的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
